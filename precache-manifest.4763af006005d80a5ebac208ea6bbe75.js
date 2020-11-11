6a7f3defbf542f4a35b009b3595e6c0acc298235
vineetk130699 <vineet@urbanstop.co>
2020-11-11 11:47:05 +0530

    Updates

---
+++ precache-manifest.4763af006005d80a5ebac208ea6bbe75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985e25ee179c229220a13760fc2bd8ac",
+    "url": "/react-books-store/index.html"
+  },
+  {
+    "revision": "520cf53cc988ed4d918c",
+    "url": "/react-books-store/static/css/main.5c93e762.chunk.css"
+  },
+  {
+    "revision": "61ad2d0f46d7905bbf44",
+    "url": "/react-books-store/static/js/2.c7a793a0.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/react-books-store/static/js/2.c7a793a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520cf53cc988ed4d918c",
+    "url": "/react-books-store/static/js/main.b66bba0c.chunk.js"
+  },
+  {
+    "revision": "4a1dcddf654fb93b2614",
+    "url": "/react-books-store/static/js/runtime-main.00ec7045.js"
+  }
+]);
